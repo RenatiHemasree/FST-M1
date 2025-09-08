8c41401be96632eea556752aead98baebf8b567f
RenatiHemasree <Renati.Hema.Sree@ibm.com>
2025-09-08 11:03:49 +0530

    Add files via upload

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,7 @@
+username = input("Enter username ")
+
+userage = int(input("Enter userage "))
+print(username)
+print(userage)
+year_100 = (100 - userage) + 2025
+print(f"{username} will turn 100 in {year_100}")
---
+++ Python/Activities/Activity2.py
@@ -0,0 +1,6 @@
+num = int (input("Enter number "))
+mod = num%2 
+if mod > 0:
+    print("you picked is Odd number")
+else:
+    print("you picked is even number")
