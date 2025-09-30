cc4bbc3c60ddb54c6da37f75bb6050187857812b
RenatiHemasree <Renati.Hema.Sree@ibm.com>
2025-09-30 10:59:47 +0530

    Add files via upload

---
+++ Cucumber/Activities/TsHomePageSteps.java
@@ -0,0 +1,32 @@
+package stepDefinitions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.openqa.selenium.By;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+public class TsHomePageSteps extends BaseClass {
+	
+	@Given("the user is on the TS homepage")
+	public void openHomePage() {
+		driver.get("https://training-support.net");
+		assertEquals(driver.getTitle(), "Training Support");
+	}
+	
+	@When("they click on the About Us link")
+	public void clickAboutLink() {
+		driver.findElement(By.linkText("About Us")).click();
+	}
+	
+	@Then("they are redirected to the About Us page")
+	public void verifyAboutPage() {
+		
+		
+		String pageHeading = driver.findElement(By.cssSelector("h1.text-center")).getText();
+		
+		System.out.println("New page title is: " + pageHeading);
+		assertEquals(pageHeading, "About Us");
+	}
+}
